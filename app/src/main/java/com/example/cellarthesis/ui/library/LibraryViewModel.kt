114b7df8f50f456ea2dfbbf19fbac5e5af48534b
Naglis Bukauskas <naglisvbukauskas@gmail.com>
2021-02-22 23:06:26 -0500

    coroutine test - working

--- app/src/main/java/com/example/cellarthesis/ui/library/LibraryViewModel.kt
+++ app/src/main/java/com/example/cellarthesis/ui/library/LibraryViewModel.kt
@@ -3,6 +3,10 @@ package com.example.cellarthesis.ui.library
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import okhttp3.*
+import java.io.IOException
 
 class LibraryViewModel : ViewModel() {
 
@@ -11,12 +15,29 @@ class LibraryViewModel : ViewModel() {
 
     //Thread the api call here through co-routines
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is the library Fragment"
-    }
-    val text: LiveData<String> = _text
+   fun fetchData() {
+       viewModelScope.launch {
+           println("Attempting to fetch JSON")
 
-    fun returnNum(): Int {
-        return 6
-    }
+           val url = "https://unogsng.p.rapidapi.com/genres"
+           val client = OkHttpClient()
+           val request = Request.Builder()
+                   .url("https://unogsng.p.rapidapi.com/genres")
+                   .get()
+                   .addHeader("x-rapidapi-key", "f174346b7amsh8eea6a1f1dd6b88p1a04c4jsna38d165ebdcd")
+                   .addHeader("x-rapidapi-host", "unogsng.p.rapidapi.com")
+                   .build()
+           val response = client.newCall(request).enqueue(object: Callback {
+               override fun onFailure(call: Call, e: IOException) {
+                   println("failure")
+               }
+
+               override fun onResponse(call: Call, response: Response) {
+                   val body = response.body?.string()
+                   println(body)
+               }
+           })
+       }
+
+   }
 }
